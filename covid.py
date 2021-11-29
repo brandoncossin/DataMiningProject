6a0c55a72f750c5e032222ab5baec7eca3bd573a
Brandon Cossin <72531057+brandoncossin@users.noreply.github.com>
2021-11-28 19:22:08 -0500

    Update covid.py

--- covid.py
+++ covid.py
@@ -3,37 +3,34 @@ from sklearn import tree
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
-from sklearn.datasets import load_iris
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import train_test_split # Import train_test_split function
-import graphviz as graphviz
-
 #load in the data
 df = pd.read_csv('parsed_covid.csv', index_col=0)
 #convert to a dataframe
 #prints first 5 then the column names
 print("* df.head()", df.head(10), sep="\n", end="\n\n")
 print(df.columns)
+
 #dummies are "fake" data aka numerically represnting non numeric data
 #hot_data = pd.get_dummies(df[['sex', 'age_group', 'race_ethnicity_combined']])
-hot_data = pd.get_dummies(df[['age_group']])
+hot_data = pd.get_dummies(df[['sex', 'age_group']])
 print("* hot_data.head()", hot_data.head(10), sep="\n", end="\n\n")
 print(hot_data.columns)
-df = df.loc[(df['sex'] != 'Other')]
-df.to_csv('new_parsed_covid.csv')
+
 #feature columns what are independent
 #feature_cols = ['sex' , 'age_group', 'race_ethnicity_combined']
 #x = df[feature_cols]
 #dependent value the target value
 #y = df['death_yn']
 # training data can be seperated into percentages. some for test and some for training of the model
-#X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=1) # 70% training and 30% test
+X_train, X_test, y_train, y_test = train_test_split(hot_data, df['death_yn'], test_size=0.3, random_state=1) # 70% training and 30% test
 
 # Create Decision Tree classifer object
 clf = DecisionTreeClassifier(class_weight='balanced')
 
 # Train Decision Tree Classifer
-clf = clf.fit(hot_data, df['death_yn'])
+clf = clf.fit(X_train, y_train)
 tree.plot_tree(clf)
 text_representation = tree.export_text(clf)
 print(text_representation)
@@ -45,7 +42,9 @@ cn = ['no', 'yes']
 fig = tree.plot_tree(clf, 
                    feature_names=hot_data.columns,  
                    #class_names=df['death_yn'],
-                   class_names=cn,
-                   filled=True)
-plt.savefig('books_read.png')
+                   class_names=['unprobable death', 'probable death'],
+                   filled=True,
+                   rounded=True)
+plt.savefig('covid_graph.pdf')
+plt.savefig('covid_graph.svg')
 plt.show()
